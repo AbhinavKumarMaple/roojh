d70830cff9e0578aa4fda1caa00ff39cd1e8ab01
AbhinavKumarMaple <abhinavmaple@gmail.com>
2024-07-26 18:21:28 +0530

    init and added pages

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DoctorPage from './pages/DoctorPage';
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/doctor" element={<DoctorPage/>} />
+    </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
